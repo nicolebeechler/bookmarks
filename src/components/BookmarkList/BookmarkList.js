3f0ccf201fbf1cd4dbca4b006834abb685168228
Nicole Beechler <nmbeechler@gmail.com>
2024-02-19 22:39:32 -0500

    Bookmark components

---
+++ src/components/BookmarkList/BookmarkList.js
@@ -0,0 +1,57 @@
+import styles from './BookmarkList.module.scss'
+import Bookmark from '../Bookmark/Bookmark'
+
+ 
+export default function BookmarkList ({ 
+    newBookmark, 
+    createBookmark, 
+    setNewBookmark, 
+    allBookmarks,
+    deleteBookmark,
+    updateBookmark
+}){
+    return(
+        <div className={styles.bookmarklist}>
+            <h5>Add New Bookmark:</h5>
+            <div className={styles.inputs}>
+            <input 
+            className={styles.input}
+            id="bookmark-title-input"
+            type="text" 
+            value={newBookmark.title}
+            placeholder="website"
+            onChange={(e) => {
+                setNewBookmark({...newBookmark, title: e.target.value, url: document.getElementById("bookmark-url-input").value})
+            }}
+            />
+            <input
+            className={styles.input}
+            id="bookmark-url-input"
+            type="text"
+            value={newBookmark.url}
+            placeholder="http://"
+            onChange={(e) => {
+                setNewBookmark({...newBookmark, title: document.getElementById("bookmark-title-input").value, url: e.target.value})
+              }}
+            />
+            <button 
+            className={styles.button}
+                onClick={() => {
+                createBookmark()
+            }} 
+            >Add
+            </button>
+            </div>
+            
+        {allBookmarks.map(bookmark => (
+            <Bookmark 
+                key={bookmark._id} 
+                bookmark={bookmark}
+                inputAction ={updateBookmark} 
+                buttonAction={deleteBookmark}
+                buttonText={'X'}
+            />
+        ))}
+        </div>
+    )
+}
